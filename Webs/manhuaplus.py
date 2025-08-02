77b0cc1cfe98049b7ccc64b2d01b6473da45bea1
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-08-02 18:01:09 +0530

    ʀ ᴇ x ʏ

--- Webs/manhuaplus.py
+++ Webs/manhuaplus.py
@@ -47,10 +47,10 @@ class ManhuaplusWebs(Scraper):
 
   async def get_chapters(self, data, page: int=1):
     results = data
-    results['msg'] = f"<b>{results['title']}</b>\n\n"
-    results['msg'] += f"<b>Url:</b> {results['url']}\n\n"
-    results['msg'] += f"<b>Lastest:</b> <code>{results['last']}</code>\n"
-    results['msg'] += f"<b>Description:</b> <code>{results['description']}</code>\n"
+    results['msg'] = f"<b><blockquote>◈ {results['title']}</b></blockquote>\n──────────────────────────\n"
+    results['msg'] += f"<b><blockquote>Sɪᴛᴇ ᴜʀʟ : {results['url']}</b></blockquote>\n──────────────────────────\n"
+    results['msg'] += f"<b><blockquote>Lᴀsᴛᴇsᴛ : {results['last']}<b><blockquote>\n──────────────────────────\n"
+    results['msg'] += f"<b><blockquote>Dᴇsᴄʀɪᴘᴛɪᴏɴ : {results['description']}<b><blockquote>\n"
     
     content = await self.get(results['url'], cs=True)
     bs = BeautifulSoup(content, "html.parser") if content else None
